5898d949f62af10dbd305ecdf183c930a64c2b93
ienjir <info@ienjir.dev>
2025-06-03 22:17:20 +0200

    Added comments for token

--- token/token.go
+++ token/token.go
@@ -11,12 +11,15 @@ const (
 	ILLEGAL = "ILLEGAL"
 	EOF = "EOF"
 
+	// Identifiers + literals
 	IDENT = "IDENT"
 	INT = "INT"
 
+	// Operators
 	ASSIGN = "="
 	PLUS = "+"
 
+	// Delimiters
 	COMMA = ","
 	SEMICOLON = ";"
 
@@ -25,6 +28,7 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
+	// Keywords
 	FUNCTION = "FUNCTION"
 	LET = "LET"
 )
