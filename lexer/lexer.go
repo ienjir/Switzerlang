8225f85c656fe1e09a20db093c8ff65f03b5e9f3
ienjir <info@ienjir.dev>
2025-06-10 20:14:35 +0200

    Added read char function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,3 +11,14 @@ func New(input string) *Lexer {
 	lexer := &Lexer{input: input}
 	return lexer
 }
+
+func (lexer *Lexer) ReadChar() {
+	if lexer.readPosition >= len(lexer.input) {
+		lexer.char = 0
+	} else {
+		lexer.char = lexer.input[lexer.readPosition]
+	}
+
+	lexer.position = lexer.readPosition
+	lexer.readPosition ++
+}
